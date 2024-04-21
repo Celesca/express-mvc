cadf6c15299eb45e22d21de5d069a30c801cb854
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-21 11:55:16 +0700

    add: mysql connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,54 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from "dotenv";
+import mysql from "mysql2/promise";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/testdb', async (req: Request, res: Response) => {
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'yourdb',
+        port: 3306
+    }).then((conn) => {
+        conn.query('SELECT * FROM users')
+        .then((results) => {
+            res.json(results[0]);
+        })
+    })
+    .catch((error) => {
+        console.error('Error connecting to database: ', error)
+        res.status(500).send('Error connecting to database')
+    })
+})
+
+app.get('/test-db-new' , async(req: Request, res: Response) => {
+    try {
+        const conn = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'yourdb',
+            port: 3306
+        })
+    
+        const results = await conn.query('SELECT * FROM users')
+        res.json(results[0])
+
+    } catch (error: any)
+     {
+        console.error('Error fetching users:' , error.message)
+        res.status(500).send('Error fetching users')
+    }
+
+})
 
 app.get("/", (req:Request, res:Response) => {
     res.send("Express + TypeScript Server");
