9267edb10995c8fc1da4728f0d3c02dedfce5864
Sawit Koseeyaumporn <folk.sawit@gmail.com>
2024-04-22 15:19:01 +0700

    add: Mysql Connection

--- pkg/user/controller/UserController.ts
+++ pkg/user/controller/UserController.ts
@@ -3,9 +3,7 @@ import UserService from "../service/UserService";
 
 async function GetAllLotteries(req: Request, res: Response) {
     const Lotteries:any = await UserService.GetAllLotteries();
-    console.log(Lotteries);
-    const results = Lotteries
-    res.json(results)
+    res.json(Lotteries);
 }
 
 export default { GetAllLotteries }
